dbe6c585064cc0a100fdcd24e1fd9820d5613eac
Yoann FOREAU <yforeau@e1r12p6.42.fr>
2019-03-14 09:49:37 +0100

    Add main and ms_data functions
    The main consists of an infinite loop on ms_data functions. Minishell
    only exits when the user types exit in the console.

--- includes/ms_data.h
+++ includes/ms_data.h
@@ -6,20 +6,29 @@
 /*   By: yforeau <yforeau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/14 09:00:45 by yforeau           #+#    #+#             */
-/*   Updated: 2019/03/14 09:05:26 by yforeau          ###   ########.fr       */
+/*   Updated: 2019/03/14 09:45:09 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MS_DATA
 # define MS_DATA
 
+# define ENV_SIZE	256
+
 typedef struct	s_ms_data
 {
-	char		**env;
-	char		*st_env[ENV_SIZE];
-	char		**al_env;
-	char		*input_buffer;
-	char		**command;
+	char		**env;					/*environment*/
+	int			envc;					/*count env vars (max = 255)*/
+	char		*st_env[ENV_SIZE];		/*static env*/
+	char		**al_env;				/*allocated env*/
+	char		*path;					/*PATH string*/
+	char		*input_buffer;			/*raw input*/
+	char		**command;				/*after parsing*/
 }				t_ms_data;
+
+void	ms_init(t_ms_data *msd, char **env);
+void	ms_input(t_ms_data *msd);
+void	ms_parse(t_ms_data *msd);
+void	ms_execute(t_ms_data *msd);
 
 #endif
---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include "ms_data.h"
+
+int	main(int argc, char **argv, char **env)
+{
+	t_ms_data	msd;
+
+	ms_init(&msd, env);
+	while (1)
+	{
+		ms_input(&msd);
+		if (msd.input_buffer)
+			ms_parse(&msd);
+		if (msd.command)
+			ms_execute(&msd);
+	}
+	return (0);
+}
