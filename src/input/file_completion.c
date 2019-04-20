9e0e5bc9d87d1ff1cca320b5232e281a81584894
yoann foreau <yoannforeau@yoanns-MacBook-Pro.local>
2019-04-20 19:59:12 +0200

    Fix file_completion when it is a local file
    get_path was modifying the part variable all the time, even when
    it did not need to.

--- src/input/file_completion.c
+++ src/input/file_completion.c
@@ -6,13 +6,13 @@
 /*   By: yforeau <yforeau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/20 18:29:46 by yforeau           #+#    #+#             */
-/*   Updated: 2019/04/20 19:28:00 by yforeau          ###   ########.fr       */
+/*   Updated: 2019/04/20 19:55:57 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tab_completion.h"
 
-static char	*get_path(char *part)
+static char	*get_path(char **path, char *part)
 {
 	char	*sl;
 	char	*ptr;
@@ -22,9 +22,13 @@ static char	*get_path(char *part)
 	while ((ptr = ft_strchr(ptr + 1, '/')))
 		sl = ptr;
 	if (!sl)
-		return (ft_strdup("."));
+		*path = ft_strdup(".");
 	else
-		return (ft_strsub(part, 0, (sl - part) + 1));
+	{
+		*path = ft_strsub(part, 0, (sl - part) + 1);
+		part = sl + 1;
+	}
+	return (part);
 }
 
 char		*file_completion(char *part)
@@ -36,8 +40,7 @@ char		*file_completion(char *part)
 	t_list	*files;
 
 	comp = NULL;
-	path = get_path(part);
-	part += ft_strlen(path);
+	part = get_path(&path, part);
 	len = ft_strlen(part);
 	if (*part && *part != '/')
 	{
@@ -52,6 +55,7 @@ char		*file_completion(char *part)
 			ft_lst_discard(NULL, &files);
 		}
 	}
+	//check_dir(path, part, comp);
 	ft_memdel((void **)&path);
 	return (comp);
 }
