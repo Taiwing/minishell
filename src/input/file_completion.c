e2dc4873936e2743efbccf11ee1675605407c4b7
yoann foreau <yoannforeau@yoanns-MacBook-Pro.local>
2019-04-20 20:27:59 +0200

    Add the trailing '/' when completing a directory
    file_completion now checks at the end of the completion if the
    resulting string is a directory. If it is, it adds a trailing
    '/', because it's cooler that way.

--- src/input/file_completion.c
+++ src/input/file_completion.c
@@ -6,10 +6,11 @@
 /*   By: yforeau <yforeau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/20 18:29:46 by yforeau           #+#    #+#             */
-/*   Updated: 2019/04/20 19:55:57 by yforeau          ###   ########.fr       */
+/*   Updated: 2019/04/20 20:26:39 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <sys/stat.h>
 #include "tab_completion.h"
 
 static char	*get_path(char **path, char *part)
@@ -17,7 +18,7 @@ static char	*get_path(char **path, char *part)
 	char	*sl;
 	char	*ptr;
 
-	sl = NULL;
+	sl = *part == '/' ? part : NULL;
 	ptr = part;
 	while ((ptr = ft_strchr(ptr + 1, '/')))
 		sl = ptr;
@@ -29,6 +30,26 @@ static char	*get_path(char **path, char *part)
 		part = sl + 1;
 	}
 	return (part);
+}
+
+static void	check_dir(char *path, char *part, char **comp)
+{
+	struct stat	st;
+	char		*stpath;
+
+	stpath = NULL;
+	if (!*part || !*comp)
+		return ;
+	if (!ft_strcmp(path, "."))
+		stpath = ft_strjoin(part, *comp);
+	else
+	{
+		stpath = ft_strjoin(path, part);
+		stpath = ft_stradd(&stpath, *comp, ft_strlen(*comp));
+	}
+	if (!stat(stpath, &st) && ((st.st_mode & S_IFMT) == S_IFDIR))
+		*comp = ft_stradd(comp, "/", 1);
+	ft_memdel((void **)&stpath);
 }
 
 char		*file_completion(char *part)
@@ -55,7 +76,7 @@ char		*file_completion(char *part)
 			ft_lst_discard(NULL, &files);
 		}
 	}
-	//check_dir(path, part, comp);
+	check_dir(path, part, &comp);
 	ft_memdel((void **)&path);
 	return (comp);
 }
