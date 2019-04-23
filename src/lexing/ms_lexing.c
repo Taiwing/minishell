94355ead81c5f1be2793dc554ab1d9a980dbae6b
Yoann FOREAU <yforeau@e2r1p13.42.fr>
2019-04-23 10:47:23 +0200

    Do not save multiline commands in history
    This causes too much trouble.

--- src/lexing/ms_lexing.c
+++ src/lexing/ms_lexing.c
@@ -6,7 +6,7 @@
 /*   By: yforeau <yforeau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/10 22:02:54 by yforeau           #+#    #+#             */
-/*   Updated: 2019/04/23 08:17:37 by yforeau          ###   ########.fr       */
+/*   Updated: 2019/04/23 10:18:57 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,18 @@
 
 t_list		*ms_lexing(t_ms_data *msd, char **input)
 {
+	char	*nl;
 	t_list	*lst;
 
 	lst = NULL;
-	if (ft_strlen(*input) > 1)
+	*input = join_lines(*input, NO_QUOTE);
+	if (*input && ft_strlen(*input) > 1
+		&& (!(nl = ft_strchr(*input, '\n')) || !nl[1]))
 	{
 		add_to_history(*input, &msd->hist, get_shvar_val("HOME", msd->env),
 			&msd->cmd_c);
 		++msd->cmd_c;
 	}
-	*input = join_lines(*input, NO_QUOTE);
 	if (*input)
 	{
 		lst = tokenize(*input);
