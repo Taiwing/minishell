9ade37bda4b85df65fbd825848ff1046bf9a349d
Yoann FOREAU <yforeau@e3r11p8.42.fr>
2019-04-29 15:57:19 +0200

    Normify main and ms_init

--- src/ms_init.c
+++ src/ms_init.c
@@ -6,7 +6,7 @@
 /*   By: yforeau <yforeau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/14 10:30:45 by yforeau           #+#    #+#             */
-/*   Updated: 2019/04/29 10:46:52 by yforeau          ###   ########.fr       */
+/*   Updated: 2019/04/29 15:56:16 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 #include "t_shvar.h"
 #include "ms_signals.h"
 
-void		set_custom_input_mode(void)
+void	set_custom_input_mode(void)
 {
 	struct termios			new_tattr;
-	
+
 	if (tcgetattr(0, &new_tattr) == -1)
 		ft_exit("failed to get terminal attributes", EXIT_FAILURE);
 	new_tattr.c_lflag &= ~(ICANON | ECHO | ISIG);
@@ -28,7 +28,7 @@ void		set_custom_input_mode(void)
 		ft_exit("failed to set terminal attributes", EXIT_FAILURE);
 }
 
-void		reset_input_mode(void)
+void	reset_input_mode(void)
 {
 	static struct termios	*old_tattr = NULL;
 
@@ -39,11 +39,11 @@ void		reset_input_mode(void)
 			ft_exit("failed to get terminal attributes", EXIT_FAILURE);
 		set_custom_input_mode();
 	}
-	else 
-		tcsetattr(0, TCSANOW, old_tattr); 
+	else
+		tcsetattr(0, TCSANOW, old_tattr);
 }
 
-void			ms_init(t_ms_data *msd, char **env)
+void	ms_init(t_ms_data *msd, char **env)
 {
 	init_signals();
 	ft_exitmsg("minishell");
@@ -57,7 +57,7 @@ void			ms_init(t_ms_data *msd, char **env)
 	msd->env = env_to_list(env);
 	msd->path = ft_strsplit(get_shvar_val("PATH", msd->env), ':');
 	msd->cmd_exit = 0;
-	msd->process_id = 0;	//TEMP (see how to get this shit)
+	msd->process_id = 0;
 	load_history(&msd->hist, get_shvar_val("HOME", msd->env));
 	msd->cmd_c = 0;
 }
