f68c6300d335b9c72b102a121504f9379e655adf
Yoann FOREAU <yforeau@e2r9p6.42.fr>
2019-05-02 23:01:33 +0200

    Get terminal width and cursor position on SIGWINCH

--- src/global/ms_signals.c
+++ src/global/ms_signals.c
@@ -6,7 +6,7 @@
 /*   By: yforeau <yforeau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/13 01:33:16 by yforeau           #+#    #+#             */
-/*   Updated: 2019/05/02 21:46:22 by yforeau          ###   ########.fr       */
+/*   Updated: 2019/05/02 23:01:12 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 #include "libft.h"
 #include "terminal_size.h"
 #include "terminal_cursor.h"
+
+//TEST
+#include "ms_input.h"
 
 pid_t	stat_pid(pid_t in)
 {
@@ -29,22 +32,32 @@ pid_t	stat_pid(pid_t in)
 void	signal_hand(int sig)
 {
 	pid_t	cp;
-	int		*ptr;
+	int		*tw;
+	int		*pos;
 
 	if ((cp = stat_pid(0)))
 	{
 		kill(cp, sig);
 		if (sig == SIGINT)
 			ft_putchar_fd('\n', 0);
-		else if (sig == SIGWINCH)
+	}
+	if (sig == SIGWINCH)
+	{
+		//update term width
+		if ((tw = term_width_container(NULL)))
+			*tw = get_terminal_width();
+		//update cursor position
+		if ((pos = curs_pos_container(NULL)))
+			get_curs_pos(pos);
+		//TEST
+		if (tw && pos)
 		{
-			//update term width
-			if ((ptr = term_width_container(NULL)))
-				*ptr = get_terminal_width();
-			//update cursor position
-			if ((ptr = curs_pos_container(NULL)))
-				get_curs_pos(ptr);
+			ft_printf("\033[1;1H");
+			ft_printf("terminal_width = %d\n", *tw);
+			ft_printf("cursor: X = %d; Y = %d\n", pos[X], pos[Y]);
+			ft_printf("\033[%d;%dH", pos[Y], pos[X]);
 		}
+		//TEST
 	}
 }
 
